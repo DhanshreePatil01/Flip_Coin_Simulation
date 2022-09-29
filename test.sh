44699a8fc94f18f70cd75409ed2da98dac0eebcf
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-09-29 09:13:21 +0530

    shows the no of times head and tail has won

--- test.sh
+++ test.sh
@@ -1,9 +1,29 @@
 #!bin/bash
 
-flip=$((RANDOM%2))
-if [ $flip -eq 0 ]
+head_count=0
+tail_count=0
+
+for((i=1;i<=5;i++))
+do
+  flip=$((RANDOM%2))
+   if [ $flip -eq 0 ]
+    then
+	echo "$i Head"
+        ((head_count++))
+    else
+	echo "$i Tail"
+        ((tail_count++))
+   fi
+done
+
+echo "Head count : $head_count"
+echo "Tail count : $tail_count"
+
+if((head_count>tail_count))
 then
-	echo "Head wins"
+   echo "Winner : Head"
 else
-	echo "Tail wins"
+   echo "Winner : Tails"
 fi
+
+
